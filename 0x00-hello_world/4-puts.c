aaa7803306991f4e314b54391752f3285639fc25
v3r15 <verisimuselveris@gmail.com>
2022-09-08 09:41:24 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,9 @@
  * * Description: prints "Programming is like building a multilingual puzzle"
  * *
  * * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
