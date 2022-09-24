88602a92ff674932ae79e01ce7997c119f248212
v3r15 <verisimuselveris@gmail.com>
2022-09-24 09:32:16 -0700

    This is really fucking my day

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two numbers from input
+ * @a: first parameter
+ * @b: Second parameter
+ *
+ * Description: adds two numbers
+ * Return: Always 0
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * times_table - a function that prints the 9 times table, starting with 0
- * rone = row, cone - column, d = digits of current result
- * Return: times table
- * add extra space past single digit
+* * times_table - a function that prints the 9 times table, starting with 0
+* * rone = row, cone - column, d = digits of current result
+* * Return: times table
+* * add extra space past single digit
 */
 
 void times_table(void)
