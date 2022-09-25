1f8faf8b78b7948ed5ad83d336fbaceb85a87b16
v3r15 <verisimuselveris@gmail.com>
2022-09-24 20:51:04 -0700

    A pain in the ass

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,25 +10,29 @@ void times_table(void)
 {
 	int r, c, d;
 
-	for (r = 0; c <= 9; r++)
+	for (r = 0; r <= 9; r++)
 	{
 		_putchar('0');
-
+		_putchar(',');
+		_putchar(' ');
 		for (c = 1; c <= 9; c++)
+		{
+			d = (r * c);
+		}
+		if ((d / 10) > 0)
+		{
+			_putchar((d / 10) + '0');
+		}
+		else
+		{
+			_putchar(' ');
+		}
+			_putchar((d % 10) + '0');
+		if (c < 9)
 		{
 			_putchar(',');
 			_putchar(' ');
-
-			d = (r * c);
-
-			if (d <= 9)
-				_putchar(' ');
-			else
-				_putchar((d / 10) + '0');
-
-			_putchar((d % 10) + '0');
 		}
-		_putchar('\n');
 	}
+		_putchar('\n');
 }
-
