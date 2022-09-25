0a1e52ac0ab8be4246a75e95d356274cea98e718
v3r15 <verisimuselveris@gmail.com>
2022-09-24 22:28:28 -0700

    Another pain

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#define LARGEST 10000000000
+#define LARGEST 1000000000
 /**
  * main - main block
  * Description: Finad and print the first 98 fib numbers
@@ -9,32 +9,37 @@
 
 int main(void)
 {
-	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0; bk2 = 2;
-	unsigned long int hold1, hold2, hold3;
-	int count;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	printf("%lu, %lu, ", bk1, bk2);
-	for (count = 2; count < 98; count ++)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
-		{
-			hold1 = (bk1 + bk2) / LARGEST;
-			hold2 = (bk1 + bk2) % LARGEST;
-			hold3 = fr1 + fr2 + hold1;
-			fr1 = fr2, fr2 = hold3;
-			bk1 = bk2, bk2 = hold2;
-			printf("%lu%010lu", fr2, bk2);
-		}
-		else
-		{
-			hold2 = bk1 + bk2;
-			bk1 = bk2, bk2 = hold2;
-			printf("%lu", bk2);
-		}
-		if (count != 97)
-			printf(", ");
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
+
+	j1 = j / LARGEST;
+	j2 = j % LARGEST;
+	k1 = k / LARGEST;
+	k2 = k % LARGEST;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / LARGEST));
+		printf("%lu", k2 % LARGEST);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
 	printf("\n");
+
 	return (0);
 }
 
